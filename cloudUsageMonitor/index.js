b0512df59a0d97014001a44eaea0eac33ae19586
NickVanBommel <nvanbomm@uwo.ca>
2019-03-29 12:23:03 -0400

    fixing up cloud usage monitor

--- cloudUsageMonitor/index.js
+++ cloudUsageMonitor/index.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express();
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 
-var vim = require('./routes/vim');
-var vms = require('./routes/vms');
 var cloudUsageMonitor = require('./routes/cloudUsageMonitor');
 var events = require('./routes/events');
-var home = require('./routes/home');
 var methodOverride = require('method-override')
 
 app.use(methodOverride('_method'))
@@ -17,8 +14,5 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.use('/api/vim', vim);
-app.use('/api/vms', vms);
 app.use('/api/cloud-usage-monitor', cloudUsageMonitor);
 app.use('/api/events', events);
-app.use('/home', home);
