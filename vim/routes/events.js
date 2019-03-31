78897bd9b277a9677e8c26b09e46687b56723e6a
Tom Machado <tmachad3@gmail.com>
2019-03-31 19:53:40 -0400

    Added http to axios call

--- vim/routes/events.js
+++ vim/routes/events.js
@@ -11,7 +11,7 @@ const usageMonitorEndpt = process.env.USAGE_MONITOR_ENDPOINT;
 router.route('/usage/:vmId')
     .get(parseUrlencoded, parseJSON, (req, res) => {
         try {
-            axios.get('10.0.0.7/api/events/usage/' + req.params.vmId)
+            axios.get('http://10.0.0.7:80/api/events/usage/' + req.params.vmId)
 			.then(function (resp) {
 				console.log(resp.data);
 				res.send(resp.data);
@@ -27,7 +27,7 @@ router.route('/usage/:vmId')
 router.route('/cost/:ccid')
     .get(parseUrlencoded, parseJSON, (req, res) => {
         try {
-			axios.get('10.0.0.7/api/events/cost/' + req.params.ccid)
+			axios.get('http://10.0.0.7:80/api/events/cost/' + req.params.ccid)
 			.then(function (resp) {
 				console.log(resp.data);
 				res.send(resp.data);
