51c2828b0d7a9a030e7107fcaa26c1ae24350865
NickVanBommel <nvanbomm@uwo.ca>
2019-03-31 20:30:19 -0400

    adding test to database

--- cloudUsageMonitor/database.js
+++ cloudUsageMonitor/database.js
@@ -12,4 +12,9 @@ var pool  = mysql.createPool({
   timezone        : 'Z'
 });
 
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
 module.exports = pool;
