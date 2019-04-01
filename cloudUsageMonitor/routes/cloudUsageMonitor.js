5b59dcfdef5f3c43c3b1aa93d8dc65197634739d
NickVanBommel <nvanbomm@uwo.ca>
2019-03-31 20:55:11 -0400

    fixed datetime

--- cloudUsageMonitor/routes/cloudUsageMonitor.js
+++ cloudUsageMonitor/routes/cloudUsageMonitor.js
@@ -26,7 +26,7 @@ router.route('/')
         var eventParams = [vmID, ccID, vmConfig, eventType, timestamp];
 
         try {
-            db.query('INSERT INTO events (VMID, userID, VMConfigTypeID, eventType, eventTimeStamp) VALUES (?, ?, ?, ?, ?)', eventParams, (insertError, insertResult) => {
+            db.query('INSERT INTO events (VMID, userID, VMConfigTypeID, eventType, NOW()) VALUES (?, ?, ?, ?)', eventParams, (insertError, insertResult) => {
                 if (insertError) {
                     throw insertError;
                 }
